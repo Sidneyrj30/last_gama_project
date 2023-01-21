9c1cb7c4ecf9168a0e8334747763341cd6304087
Sidney Júnior <junior.lopes@luizalabs.com>
2023-01-21 19:47:38 -0300

    mudança exerc 7 e exerc 8 feito

--- exerc_7.py
+++ exerc_7.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# curl -X POST 'http://localhost:5000/soma'-H 'Content-Type: application/json' -d '{"x": 1, "y": 2}'
+# curl -X POST 'http://localhost:5000/soma' -H 'Content-Type: application/json' -d '{"x": 1, "y": 2}'
 
 @app.route('/soma', methods=["POST"])
 def operacao():
---
+++ exerc_8.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+num = {"numero": 0}
+
+@app.route('/contador', methods=["GET"])
+def conta():
+    return num, 200
+
+@app.route('/contador', methods=["POST"])
+def operacao():
+    global num
+    num = request.json
+    return num, 200
+
+@app.route('/contador/incrementa', methods=["PUT"])
+def incrementa():
+    num["numero"] += 1
+    return num, 202  
+
+@app.route('/contador', methods=["DELETE"])
+def deletar():
+    num["numero"] = 0
+    return num, 202  
+
+if __name__ == "__main__":
+    app.run(debug=True)
