a2c8b48a3e9ecadc3a67ff137be903a6f3c0caec
Sidney Júnior <junior.lopes@luizalabs.com>
2023-01-22 16:00:58 -0300

    ultimo exercicio

--- exerc_6.py
+++ exerc_6.py
@@ -1,12 +1,16 @@
-sku = "" 
-estoque = 0 
-def ajustar_estoque():
-    print("")
-    # db.produto.update(
-    # {
-    # sku: sku
-    # },
-    # {
-    # $inc: estoque
-    # }
-    # )
+from pymongo import MongoClient
+
+url_conexao = 'mongodb+srv://last_project_gama:gama_1234@cluster0.jruk8oe.mongodb.net/test'
+conn = MongoClient(url_conexao)
+collection = conn.gama.produto
+
+def ajustar_estoque(sku, estoque, collection):
+    collection.update_one({"sku":sku}, {"$set":{"estoque": estoque}})
+
+print("Alteração de estoque:")
+sku = input("Digite o sku: ")
+estoque = int(input("Digite o novo estoque: "))
+
+ajustar_estoque(sku, estoque, collection)
+
+
